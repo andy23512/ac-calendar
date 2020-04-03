c57a5be194fae61e2fb62fb3a1c16f47e1d0699a
andy23512 <andy23512@gmail.com>
2020-04-03 13:57:23 +0800

    Initial commit from akimbo-cli

---
+++ frontend/apps/app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule, HttpClient, HttpClientXsrfModule } from '@angular/common/http';
+
+export function getCsrf(http: HttpClient) {
+  return () =>
+    http
+      .get('/api/get_csrf')
+      .toPromise();
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, GraphQLModule, HttpClientModule, HttpClientXsrfModule.withOptions({cookieName: 'ac-calendar-csrf', headerName: 'X-CSRFToken'})],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: getCsrf,
+      multi: true,
+      deps: [HttpClient],
+    },
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
